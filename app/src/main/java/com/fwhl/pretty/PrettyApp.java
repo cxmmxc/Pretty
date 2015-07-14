d878f77e2dedede75f6919270a81f405d525f0d4
lmz_cxm <herewinner@163.com>
2015-07-14 21:51:46 +0800

    1.更改了路径的魔鬼数字 2.明儿添加积分墙功能

--- app/src/main/java/com/fwhl/pretty/PrettyApp.java
+++ app/src/main/java/com/fwhl/pretty/PrettyApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.pm.PackageManager;
 import android.os.Environment;
 
+import com.fwhl.pretty.constant.Constant;
 import com.fwhl.pretty.util.ToastAlone;
 import com.lidroid.xutils.util.LogUtils;
 
@@ -29,7 +30,7 @@ public class PrettyApp extends Application {
 
     private void initData() {
         String path = Environment.getExternalStorageDirectory().getPath();
-        File file = new File(path+"/DCIM/1024MM");
+        File file = new File(Constant.CaceFileDir);
         LogUtils.v("mkdir=" + path);
         if(!file.exists()) {
             boolean mkdir = file.mkdir();
